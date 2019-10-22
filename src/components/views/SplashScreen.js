9c8ad2b94f9bcc1b39a405964f18b0332ccc4638
duncan.laidlaw <duncan.laidlaw@deeperthanblue.co.uk>
2019-10-22 04:04:16 +0100

    scss installed

--- src/components/views/SplashScreen.js
+++ src/components/views/SplashScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 // css
-import './SplashScreen.css'
+import './SplashScreen.scss'
 
 const SplashScreen = () => {
   return (
